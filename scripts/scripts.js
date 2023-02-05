6a6f179765cee76b513612958d03b1cf5406db82
MagneticShade <senya.krivorot11@gmail.com>
2023-02-05 17:34:44 +0200

    slider wip

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,29 +4,56 @@ let warlock=document.getElementById(`warlock`);
 let hunter=document.getElementById(`hunter`);
 let titan=document.getElementById(`titan`);
 
-warlock.addEventListener("mouseover", function () {
-    warlock.style.zIndex='2';
-    warlock.style.clipPath=`polygon(0% 0%, 70% 0%, 55% 100%, 0% 100%)`;
-    hunter.style.clipPath='polygon(70% 0%, 90% 0%, 75% 100%, 55% 100%)';
+let is_anim=false;
+
+function warlock_active() {
+    warlock.style.clipPath = `polygon(0% 0%, 80% 0%, 70% 100%, 0% 100%)`;
+    hunter.style.clipPath = 'polygon(80% 0%, 95% 0%, 85% 100%, 70% 100%)';
+    titan.style.removeProperty('clip-path');
+}
+function titan_active() {
+    titan.style.clipPath=`polygon(30% 0%, 100% 0%, 100% 100%, 20% 100%)`;
+    hunter.style.clipPath='polygon(15% 0%, 30% 0%, 20% 100%, 5% 100%)';
+    warlock.style.removeProperty('clip-path');
+}
+
+function hunter_active() {
+    hunter.style.clipPath=`polygon(15% 0%, 95% 0%, 85% 100%, 5% 100%)`;
+    warlock.style.clipPath=' polygon(0% 0%, 15% 0%, 5% 100%, 0% 100%)';
+    titan.style.clipPath='polygon(95% 0%, 100% 0%, 100% 100%, 85% 100%)';
+}
+
+warlock.addEventListener("click", function () {
+
+    if (!is_anim) {
+        is_anim=true;
+       warlock_active()
+
+        setTimeout(function () {
+            is_anim=false;
+        },150)
+    }
 })
-warlock.addEventListener("mouseout",function () {
-    warlock.style.removeProperty('z-index');
-    warlock.style.removeProperty(`clip-path`);
-    hunter.style.removeProperty('clip-path')
+
+titan.addEventListener("click",function () {
+    if (!is_anim) {
+        is_anim=true;
+        titan_active();
+
+        setTimeout(function () {
+            is_anim=false;
+        },150)
+    }
 })
-titan.addEventListener("mouseover",function () {
-    titan.style.zIndex='2';
-    titan.style.clipPath=`polygon(55% 0%, 100% 0%, 100% 100%, 40% 100%)`;
-    hunter.style.clipPath='polygon(25% 0%, 55% 0%, 40% 100%, 10% 100%)';
-})
-titan.addEventListener("mouseout",function () {
-    titan.style.removeProperty('z-index');
-    titan.style.removeProperty(`clip-path`);
-    hunter.style.removeProperty('clip-path')
-})
-hunter.addEventListener("mouseover",function () {
-    hunter.style.zIndex='2';
-    hunter.style.clipPath=`polygon(25% 0%, 90% 0%, 75% 100%, 10% 100%)`;
-    warlock.style.clipPath=' polygon(0% 0%, 25% 0%, 10% 100%, 0% 100%)';
-    titan.style.clipPath='polygon(90% 0%, 100% 0%, 100% 100%, 75% 100%)';
+
+hunter.addEventListener("click",function () {
+    if (!is_anim) {
+        is_anim=true;
+       hunter_active();
+
+        setTimeout(function () {
+            is_anim=false;
+        },150)
+
+    }
 })
