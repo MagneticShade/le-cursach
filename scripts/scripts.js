6b7a5befb30994561839862b8ca0ed2e1552dcec
MagneticShade <senya.krivorot11@gmail.com>
2023-03-25 11:15:47 +0200

    cards

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -162,6 +162,7 @@ VanillaTilt.init(document.querySelectorAll('.card'));
 //     },
 //     offset:"50%"
 //   })
+
 let slide1=new Waypoint({
     element:document.getElementById(`horizontal_scroller`),
     handler:function(){
@@ -215,26 +216,213 @@ let slide1=new Waypoint({
         this.destroy()
     },
     offset:"50%"
-  });
+});
 
 let slide2 = new Waypoint({
     element: document.getElementById('slide2'),
     handler: function() {
-    anime({
-        targets:'#bookmarks_sl2',
-                    translateX: [-30,0],
-                    opacity: [0,1],
-                    easing: 'linear',
-                    duration:250
-    })
-    this.destroy();
+        anime({
+            targets:'#bookmarks_sl2',
+            translateX: [-300,0],
+            opacity: [0,1],
+            easing: 'linear',
+            duration:500
+        })
+        anime({
+            targets:'#header_sl2',
+            translateY: [-300,0],
+            opacity: [0,1],
+            easing: 'linear',
+            duration:500
+        })
+        anime({
+            targets:'#img_sl2',
+            translateX: [300,0],
+            opacity: [0,1],
+            easing: 'linear',
+            duration:500
+        })
+        anime({
+            targets:'#img_sl2',
+            translateX: [300,0],
+            opacity: [0,1],
+            easing: 'linear',
+            duration:500
+        })
+        anime({
+            targets:'#sub_header_sl2',
+            opacity: [0,1],
+            easing: 'linear',
+            duration:700
+        })
+        anime({
+            targets:'#filling_sl2',
+            opacity: [0,1],
+            easing: 'linear',
+            duration:700
+        })
+        anime({
+            targets:'#button_sl2',
+            translateY: [300,0],
+            opacity: [0,1],
+            easing: 'linear',
+            duration:500
+        })
+        this.destroy()
     },
     context: document.getElementById('horizontal_scroller'),
     offset:'5%',
     horizontal: true
-  });
+});
+
+let slide3 = new Waypoint({
+    element: document.getElementById('slide3'),
+    handler: function() {
+        anime({
+            targets:'#bookmarks_sl3',
+            translateX: [-300,0],
+            opacity: [0,1],
+            easing: 'linear',
+            duration:500
+        })
+        anime({
+            targets:'#header_sl3',
+            translateY: [-300,0],
+            opacity: [0,1],
+            easing: 'linear',
+            duration:500
+        })
+        anime({
+            targets:'#img_sl3',
+            translateX: [300,0],
+            opacity: [0,1],
+            easing: 'linear',
+            duration:500
+        })
+        anime({
+            targets:'#img_sl3',
+            translateX: [300,0],
+            opacity: [0,1],
+            easing: 'linear',
+            duration:500
+        })
+        anime({
+            targets:'#sub_header_sl3',
+            opacity: [0,1],
+            easing: 'linear',
+            duration:700
+        })
+        anime({
+            targets:'#filling_sl3',
+            opacity: [0,1],
+            easing: 'linear',
+            duration:700
+        })
+        anime({
+            targets:'#button_sl3',
+            translateY: [300,0],
+            opacity: [0,1],
+            easing: 'linear',
+            duration:500
+        })
+        this.destroy()
+    },
+    context: document.getElementById('horizontal_scroller'),
+    offset:'5%',
+    horizontal: true
+});
+
+let slide4 = new Waypoint({
+    element: document.getElementById('slide4'),
+    handler: function() {
+        anime({
+            targets:'#bookmarks_sl4',
+            translateX: [-300,0],
+            opacity: [0,1],
+            easing: 'linear',
+            duration:500
+        })
+        anime({
+            targets:'#header_sl4',
+            translateY: [-300,0],
+            opacity: [0,1],
+            easing: 'linear',
+            duration:500
+        })
+        anime({
+            targets:'#img_sl4',
+            translateX: [300,0],
+            opacity: [0,1],
+            easing: 'linear',
+            duration:500
+        })
+        anime({
+            targets:'#img_sl4',
+            translateX: [300,0],
+            opacity: [0,1],
+            easing: 'linear',
+            duration:500
+        })
+        anime({
+            targets:'#sub_header_sl4',
+            opacity: [0,1],
+            easing: 'linear',
+            duration:700
+        })
+        anime({
+            targets:'#filling_sl4',
+            opacity: [0,1],
+            easing: 'linear',
+            duration:700
+        })
+        anime({
+            targets:'#button_sl4',
+            translateY: [300,0],
+            opacity: [0,1],
+            easing: 'linear',
+            duration:500
+        })
+        this.destroy()
+    },
+    context: document.getElementById('horizontal_scroller'),
+    offset:'10%',
+    horizontal: true
+});
+
+console.log( await fetch('https://www.bungie.net/common/destiny2_content/json/ru/DestinyLoreDefinition-88e2ca87-7551-4503-a5b5-2527c4531503.json',{
+}).then(response=>{return response.json()}));
+
+let cayde=await fetch('https://www.bungie.net/common/destiny2_content/json/ru/DestinyPresentationNodeDefinition-88e2ca87-7551-4503-a5b5-2527c4531503.json',{
+}).then(response=>{return response.json()});
+
+console.log("https://www.bungie.net"+cayde['4003550471']['displayProperties']['iconSequences']['1']['frames']['0']);
+
+let card=document.getElementById('card1');
+// card.style.backgroundImage=`url("https://www.bungie.net"${cayde['4003550471']['displayProperties']['iconSequences']['1']['frames']['0']})`;
 
 
+function getImage(id){
+    return `https://www.bungie.net${cayde[id]['displayProperties']['iconSequences']['1']['frames']['0']}`
+}
+
+card.style.backgroundImage=`url(${getImage('4003550471')})`
+
+let card2=document.getElementById('card2');
+let card3=document.getElementById('card3');
+let card4=document.getElementById('card4');
+let card5=document.getElementById('card5');
+let card6=document.getElementById('card6');
+
+card2.style.backgroundImage=`url(${getImage('3303363217')})`
+card3.style.backgroundImage=`url(${getImage('28665996')})`
+card4.style.backgroundImage=`url(${getImage('2436418635')})`
+card5.style.backgroundImage=`url(${getImage('1960700009')})`
+card6.style.backgroundImage=`url(${getImage('3747262457')})`
+
+// console.log( await fetch('https://www.bungie.net/common/destiny2_content/json/ru/DestinyLoreDefinition-88e2ca87-7551-4503-a5b5-2527c4531503.json',{
+//     headers:{ 'X-API-Key':'6be74b820d6f473d906ce82516d03b90'}
+
+// }).then(response=>{return response.json()}));
 
 
 
